fac0b893daf3b5b63ab993bf5cdffa970b847781
JayRalph360 <84851293+JayRalph360@users.noreply.github.com>
2023-09-25 22:35:10 +0000

    Create 911.OnlineElection2.py

---
+++ Camp Practice/911.OnlineElection2.py
@@ -0,0 +1,43 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        self.persons = persons
+        self.times = times
+        self.leaders = []
+        leader_count = {}
+        current_leader = None
+        
+        for i in range(len(self.persons)):
+            person = persons[i]
+            time = times[i]
+            
+            if person not in leader_count:
+                leader_count[person] = 0
+            leader_count[person] += 1
+            
+            if current_leader is None or leader_count[person] >= leader_count[current_leader]:
+                current_leader = person
+            
+            self.leaders.append(current_leader)
+
+
+    def q(self, t: int) -> int:
+        l, r = 0, len(self.times) - 1
+        
+        while l <= r:
+            mid = l+(r-l)//2
+            if self.times[mid] <= t:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return self.leaders[r]
+
+
+
+
+        
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
